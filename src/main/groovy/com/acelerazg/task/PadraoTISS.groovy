3b1021c208c6e1dbcc174a6994558d1d3aa00601
Murillo Santos <murillo-santos@outlook.com.br>
2024-04-04 01:54:21 -0300

    Feat: Adicionado Task 2 buscar historico de versao dos componentes TISS

--- src/main/groovy/com/acelerazg/task/PadraoTISS.groovy
+++ src/main/groovy/com/acelerazg/task/PadraoTISS.groovy
@@ -32,4 +32,30 @@ class PadraoTISS {
 
     }
 
+    static void getHistoricoVersaoComponentesTISS() {
+
+        Document pagina = Core.getPaginaTISS()
+        Element conteudo = pagina.getElementsByClass("internal-link").get(1)
+        String link = conteudo.getElementsByTag("a").attr("href")
+
+        pagina = Core.getPagina(link)
+        conteudo = pagina.getElementsByTag("tbody").first()
+        List<Element> historico = conteudo.getElementsByTag("tr")
+
+        historico.forEach { tr ->
+            List<Element> tdLista = tr.getElementsByTag("td")
+            String competencia = tdLista.get(0).text()
+
+            List<String> anoCompetencia = competencia.tokenize("/")
+            Integer ano = Integer.parseInt(anoCompetencia[1])
+
+            if (ano >= 2016) {
+                String publicacao = tdLista.get(1).text()
+                String inicioVigencia = tdLista.get(2).text()
+                Core.salvarHistorico(competencia, publicacao, inicioVigencia)
+            }
+        }
+
+    }
+
 }

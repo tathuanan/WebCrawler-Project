d9504166842e37d38a24745ff35fa0c92f6be533
Murillo Santos <murillo-santos@outlook.com.br>
2024-04-04 22:21:47 -0300

    Feat: Criado Classe para executar as 3 tasks

--- src/main/groovy/com/acelerazg/Main.groovy
+++ src/main/groovy/com/acelerazg/Main.groovy
@@ -1,7 +1,9 @@
 package com.acelerazg
 
+import com.acelerazg.task.ExecutarTask
 
 static void main(String[] args) {
 
+    ExecutarTask.PadraoTISS()
 
 }
---
+++ src/main/groovy/com/acelerazg/task/ExecutarTask.groovy
@@ -0,0 +1,15 @@
+package com.acelerazg.task
+
+class ExecutarTask {
+
+    static void PadraoTISS() {
+
+        PadraoTISS.getUltimaVersaoComponentesTISS()
+
+        PadraoTISS.getHistoricoVersaoComponentesTISS()
+
+        PadraoTISS.getTabelaErrosEnvioANS()
+
+    }
+
+}
